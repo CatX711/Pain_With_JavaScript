f4d9fb12679a56edb8f088cac968e5c3ebf912c7
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-07 14:04:21 +0100

    Update button.js

--- js_button/button.js
+++ js_button/button.js
@@ -1,12 +1,12 @@
-const button = document.getElementById('change-color-button');
-let isDarkMode = false;
+const button = document.getElementById('change-color-button'); // connects to button using it's id
+let isDarkMode = false; // default value (means that the website is white)
 
-button.addEventListener('click', () => {
-  if (!isDarkMode) {
-    document.body.style.backgroundColor = '#222';
-    isDarkMode = true;
-  } else {
-    document.body.style.backgroundColor = '#fff';
-    isDarkMode = false;
+button.addEventListener('click', () => {    // when button clicked
+  if (!isDarkMode) { // if darkmode is false
+    document.body.style.backgroundColor = '#222'; // change colour to colour of choosing (in this case dark gray)
+    isDarkMode = true; // set darkmode to true
+  } else {  // else if darkmode is true
+    document.body.style.backgroundColor = '#fff'; // change colour back to white
+    isDarkMode = false; // set darkmode to false
   }
 });
